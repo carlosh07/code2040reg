914a97a885ae3e34ead0af552dacd6c993a3fffa
Carlos <Carlos@host-223-254.gaanriv.athens.ga.us.clients.pavlovmedia.com>
2016-10-08 20:23:18 -0400

    Updated js

--- getPost.js
+++ getPost.js
@@ -1,25 +1,9 @@
-require("jsdom").env("", function(err, window) {
-    if (err) {
-        console.error(err);
-        return;
-    }
- 
-    var $ = require("jquery")(window);
-});
+var send =  {token:"ca15df8f74b55e0abf96b1d66b53f3ed", github:"https://github.com/carlosh07/code2040reg"} // data that I will send
+var url = "http://challenge.code2040.org/api/register" // where I am sending it to
 
-var token =  "ca15df8f74b55e0abf96b1d66b53f3ed"
-var url = "http://challenge.code2040.org/api/register"
-
-
-var jqxhr = $.ajax(url)
-	.done(function() {
-    alert( "success" );
-  })
-  .fail(function() {
-    alert( "error" );
-  })
-  .always(function() {
-    alert( "complete" );
-  });
-
-  console.log(jqxhr);
+$(document).ready(function(){
+  $.post(url,send,
+        function(data,status){
+            console.log("Data: " + data + "\nStatus: " + status);
+        });// function prints out the the data response and then the status!
+}); //document.ready
