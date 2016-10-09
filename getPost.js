d89961bc1771d31b206fc796d76e768d93a52e8b
Carlos <Carlos@host-223-254.gaanriv.athens.ga.us.clients.pavlovmedia.com>
2016-10-08 21:55:39 -0400

    Updated JS File

--- getPost.js
+++ getPost.js
@@ -1,10 +1,21 @@
 //Carlos A. Hernandez
 var send =  {token:"ca15df8f74b55e0abf96b1d66b53f3ed", github:"https://github.com/carlosh07/code2040reg"} // data that I will send
 var url = "http://challenge.code2040.org/api/register" // where I am sending it to
+var exampleString =  "The Example";
+//var theArrayOfCharsBckWrds = exampleString.split("").reverse(); //Makes the string into an array then reverses that array -- (String + Array Methods)
+//var out  = theArrayOfCharsBckWrds.join(""); // Array Methods
+
+var reverseString = function(string){
+	var temp = string.split("").reverse();
+	return temp.join("");
+}
+
 
 $(document).ready(function(){
-  $.post(url,send,
+  /*$.post(url,send,
         function(data,status){
             console.log("Data: " + data + "\nStatus: " + status);
-        });// function prints out the the data response and then the status!
+        });// function prints out the the data response and then the status!  //Pt 1 */
+      
+    
 }); //document.ready
