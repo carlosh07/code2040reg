a68b9e4634e074ec66c2033820743f1e7cab610f
Carlos <Carlos@host-221-97.gaanriv.athens.ga.us.clients.pavlovmedia.com>
2016-10-11 21:52:55 -0400

    Pt4 Done

--- getPost.js
+++ getPost.js
@@ -1,31 +1,40 @@
 //Carlos A. Hernandez
 var send =  {token:"ca15df8f74b55e0abf96b1d66b53f3ed"};
-var url = "http://challenge.code2040.org/api/haystack" // where I am sending it to
+var url = "http://challenge.code2040.org/api/prefix" // where I am sending it to
 
 
 $(document).ready(function(){
     $.ajaxSetup({async:false});  //execute synchronously
-    var haystack = null;
-    var index = null;
+    var prefix = null;
+    var theListOfWords;
     $.post(url,send,
         function(data,status){
-            haystack =data;
-            console.log("Data" +  data + "\nStat: " + status);
-            for(a  = 0 ; a < haystack.haystack.length; a++){
-                if(haystack.needle == haystack[a])
-                    index = a;
-                    return;
-            } //for    
+            prefix =data.prefix;
+            theListOfWords = data.array;
+            console.log("Data" +  data + "\nStat: " + status + "\nprefix: " + prefix + "\nArray: "  + theListOfWords); 
+                
         }//function
         );//post
-        
-    url = "http://challenge.code2040.org/api/haystack/validate";
-    send = {token:"ca15df8f74b55e0abf96b1d66b53f3ed", needle:index};
-     $.post(url,send,
-        function(data,status){
-            console.log("Data" +  data + "\nStat: " + status);
-        }//function
-        );//post
+        var wordsWithoutPrefix =[];
+        console.log("\nThe prefix length is : " + prefix.length); //want to know the length just in case it changes.
+        for(a=0; a < theListOfWords.length; a++){
+            // console.log("\nPrefix is :" + prefix + "\nSubstring is :" + theListOfWords[a].substr(0,prefix.length-1) );
+            if(theListOfWords[a].substr(0,prefix.length) != prefix){
+                // console.log("\nFOUND AT " + a);
+                wordsWithoutPrefix.push(theListOfWords[a]);
+            }//if the prefix matched add
+        }//for
 
+        console.log(wordsWithoutPrefix);
+
+        var send2 = {token:"ca15df8f74b55e0abf96b1d66b53f3ed", array:wordsWithoutPrefix};                
+        var url2 = "http://challenge.code2040.org/api/prefix/validate";
+        console.log("S " + send2.array[2]);
+
+        $.post(url2,send2,
+        function(data,status){
+            console.log("\nRecieved:" + data + "\nStat: " + status);  
+        }//function
+        );//post
 
 }); //document.ready
